455c0d9175a7934b4ba19311d41d999555a5af3d
chenm_youshu <11247522@qq.com>
2022-10-20 19:47:14 +0800

    添加注释

--- data_helper_textcnn.py
+++ data_helper_textcnn.py
@@ -18,25 +18,52 @@ class CnnDataSet(Dataset):
     label: [0,1,]
     '''
     def __init__(self, data_path):
-        label2idx = config.label2idx
+        if not data_path:
+            pass
+        else:
+            label2idx = config.label2idx
+            word2idx = config.word2idx
+            self.texts = []
+            self.labels = []
+            with open(data_path) as f:
+                for line in f:
+                    line = json.loads(line)
+                    # 取出限定字数的words
+                    words = [word2idx.get(w, 1) for w in list(line["sentence"])[:config.base_config.max_seq_len]]
+                    self.labels.append(label2idx[line["label"]])
+                    # padding后续补0
+                    tmp = [0] * config.base_config.max_seq_len
+                    tmp[:len(words)] = words
+                    self.texts.append(tmp)
+            self.labels = np.array(self.labels)
+            self.texts = np.array(self.texts)
+            # index化的text及label
+            self.labels = torch.as_tensor(self.labels)
+            self.texts = torch.as_tensor(self.texts)
+
+    def get_signal_text_encode(self, texts):
+        '''
+        将text进行encode
+        '''
         word2idx = config.word2idx
-        self.texts = []
-        self.labels = []
-        with open(data_path) as f:
-            for line in f:
-                line = json.loads(line)
-                # 取出限定字数的words
-                words = [word2idx.get(w, 1) for w in list(line["sentence"])[:config.base_config.max_seq_len]]
-                self.labels.append(label2idx[line["label"]])
+
+        if type(texts)==str:
+            words = [word2idx.get(w, 1) for w in list(texts)[:config.base_config.max_seq_len]]
+            # padding后续补0
+            tmp = [0] * config.base_config.max_seq_len
+            tmp[:len(words)] = words
+            return tmp
+
+        if type(texts)==list:
+            texts_ = []
+            for text in texts:
+                words = [word2idx.get(w, 1) for w in list(text)[:config.base_config.max_seq_len]]
                 # padding后续补0
                 tmp = [0] * config.base_config.max_seq_len
                 tmp[:len(words)] = words
-                self.texts.append(tmp)
-        self.labels = np.array(self.labels)
-        self.texts = np.array(self.texts)
-        # index化的text及label
-        self.labels = torch.as_tensor(self.labels)
-        self.texts = torch.as_tensor(self.texts)
+                texts_.append(tmp)
+            return texts_
+
 
     def __len__(self):
         return len(self.texts)
