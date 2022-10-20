6cb8abe1fd2b7154a1bfee366607f145d75e806c
chenm_youshu <11247522@qq.com>
2022-10-20 14:15:34 +0800

    bert.py 添加注释

--- bert.py
+++ bert.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
+'''
+bert输出 + dropout + FC
+'''
 
 import torch
 import torch.nn as nn
 from transformers import BertModel
 
 
-
 class Bert(nn.Module):
+
     def __init__(self, config):
         super(Bert, self).__init__()
         self.bert_model = BertModel.from_pretrained("bert-base-chinese")
@@ -18,7 +21,9 @@ class Bert(nn.Module):
         outputs = self.bert_model(input_ids=input_ids,
                                   token_type_ids=token_type_ids,
                                   attention_mask=attention_mask)
+        # 输出为pool后的结果，每个句子一个embedding  [B,E]
         pooled_output = outputs[1]
         pooled_output = self.dropout(pooled_output)
         out = self.fc(pooled_output)
         return out
+
